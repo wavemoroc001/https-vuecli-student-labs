d55d88b2678216c452d2a842c615fc090cbb45ae
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2021-04-05 11:53:25 +0700

    finish lab2

--- src/App.vue
+++ src/App.vue
@@ -8,33 +8,56 @@
 
       <div class="container">
         <div class="survey-container">
-          <form action="">
+          <form action="" @submit.prevent="validateForm">
             <div>
               <h2 class="heading">
                 How was your Vue.js learning experience?
               </h2>
               <label class="label" for="name">Your Name</label>
-              <input class="input" id="name" type="text" />
+              <input class="input" id="name" type="text" v-model="username" ref="name" />
+              <h3 class="show-error" v-if="isUsernameValidate">Pleas fill your name</h3>
             </div>
 
             <div>
               <h2 class="heading">My learning experience was ...</h2>
 
               <div>
-                <input type="radio" name="rating" id="rating-poor" />
+                <input
+                  type="radio"
+                  name="rating"
+                  id="rating-poor"
+                  v-model="rating"
+                  value="poor"
+                  autofocus
+                />
                 <label class="label" for="rating-poor">Poor</label>
               </div>
 
               <div>
-                <input type="radio" name="rating" id="rating-avg" />
+                <input
+                  type="radio"
+                  name="rating"
+                  id="rating-avg"
+                  v-model="rating"
+                  value="avg"
+                  autofocus
+                />
                 <label class="label" for="rating-avg">Average</label>
               </div>
 
               <div>
-                <input type="radio" name="rating" id="rating-great" />
+                <input
+                  type="radio"
+                  name="rating"
+                  id="rating-great"
+                  v-model="rating"
+                  value="great"
+                  autofocus
+                />
                 <label class="label" for="rating-great">Great</label>
               </div>
             </div>
+            <h3 class="show-error" v-if="isRatingValidate">Choose your Experience!</h3>
             <button class="btn">
               Submit
             </button>
@@ -46,12 +69,33 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      username: null,
+      rating: null,
+      isUsernameValidate: null,
+      isRatingValidate: null,
+    };
+  },
+  methods: {
+    validateForm() {
+      //this.isUsernameValidate = this.username ? true : false;
+      this.isRatingValidate = this.rating ? false : true ;
+
+      if (this.username) this.isUsernameValidate = false;
+      else {
+        this.isUsernameValidate = true;
+        this.$refs.name.focus()
+      }
+      alert("Finished !")
+    },
+  },
+};
 </script>
