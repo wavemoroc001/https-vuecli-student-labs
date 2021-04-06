9e10683a7a1fccf838829d837388b5378900b56f
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2021-04-07 00:54:00 +0700

    Finish Lab7

--- src/App.vue
+++ src/App.vue
@@ -5,151 +5,22 @@
         <img class="logo" alt="Vue logo" src="./assets/logo.png" />
         <HelloWorld msg="Welcome to VueCLI project" />
       </div>
-
       <div class="container">
-        <div class="survey-container">
-          <form @submit.prevent="submitForm">
-            <base-card>
-              <h2 class="heading">
-                How was your Vue.js learning experience?
-              </h2>
-
-              <label class="label" for="name">Your Name</label>
-
-              <input
-                class="input"
-                :class="{ 'bg-red-50': invalidNameInput }"
-                id="name"
-                type="text"
-                v-model.trim="enteredName"
-                @blur="validateName"
-              />
-
-              <p v-if="invalidNameInput" class="text-red-500">
-                Please enter your name!
-              </p>
-            </base-card>
-
-            <base-card>
-              <h2 class="heading">My learning experience was ...</h2>
-
-              <div>
-                <input
-                  type="radio"
-                  name="rating"
-                  id="rating-poor"
-                  value="poor"
-                  v-model="rating"
-                />
-                <label class="label" for="rating-poor">Poor</label>
-              </div>
-
-              <div>
-                <input
-                  type="radio"
-                  name="rating"
-                  id="rating-avg"
-                  value="average"
-                  v-model="rating"
-                />
-                <label class="label" for="rating-avg">Average</label>
-              </div>
-
-              <div>
-                <input
-                  type="radio"
-                  name="rating"
-                  id="rating-great"
-                  value="great"
-                  v-model="rating"
-                />
-                <label class="label" for="rating-great">Great</label>
-              </div>
-              <p v-if="invalidRatingInput" class="text-red-500">
-                Please choose your learning experience!
-              </p>
-            </base-card>
-
-            <!-- <button class="btn">
-              Submit
-            </button> -->
-            <base-button
-              @btn-click="submitForm"
-              bgcolor="bg-purple-900"
-              txtcolor="text-white"
-              label="Submit"
-            ></base-button>
-          </form>
-        </div>
-      </div>
-
-      <div class="container">
-        <base-card>
-          <ul v-for="result in surveyResults" :key="result.id">
-            <li>
-              <span class="text-purple-600 italic">{{ result.name }}</span>
-              rated the learning experience
-              <span class="text-green-600 italic">{{ result.rating }}</span>
-            </li>
-          </ul>
-        </base-card>
+        <add-survet />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import AddSurvet from "./components/survey/AddSurvey.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    AddSurvet,
   },
-  data() {
-    return {
-      enteredName: '',
-      rating: null,
-      invalidNameInput: false,
-      invalidRatingInput: false,
-      surveyResults: [
-        {
-          id: 1,
-          name: 'Umaporn',
-          rating: 'Great'
-        },
-        {
-          id: 2,
-          name: 'Suda',
-          rating: 'Average'
-        }
-      ]
-    }
-  },
-  methods: {
-    submitForm() {
-      // alert('submit form')
-      this.invalidNameInput = this.enteredName === '' ? true : false
-      this.invalidRatingInput = this.rating === null ? true : false
-
-      console.log(`name value: ${this.enteredName}`)
-      console.log(`rating value: ${this.rating}`)
-      console.log(`invalid name: ${this.invalidNameInput}`)
-      console.log(`invalid rating: ${this.invalidRatingInput}`)
-    },
-
-    validateName() {
-      this.invalidNameInput = this.enteredName === '' ? true : false
-      console.log(`name: ${this.invalidNameInput}`)
-    },
-    async fetchSurveyResult() {
-      const res = await fetch('http://localhost:5000/surveyResults')
-      const data = await res.json()
-      return data
-    }
-  },
-  async created() {
-    this.surveyResults = await this.fetchSurveyResult()
-  }
-}
+};
 </script>
---
+++ src/components/survey/AddSurvey.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="survey-container">
+    <form @submit.prevent="submitForm">
+      <base-card>
+        <h2 class="heading">
+          How was your Vue.js learning experience?
+        </h2>
+
+        <label class="label" for="name">Your Name</label>
+
+        <input
+          class="input"
+          :class="{ 'bg-red-50': invalidNameInput }"
+          id="name"
+          type="text"
+          v-model.trim="enteredName"
+          @blur="validateName"
+        />
+
+        <p v-if="invalidNameInput" class="text-red-500">
+          Please enter your name!
+        </p>
+      </base-card>
+
+      <base-card>
+        <h2 class="heading">My learning experience was ...</h2>
+
+        <div>
+          <input
+            type="radio"
+            name="rating"
+            id="rating-poor"
+            value="poor"
+            v-model="rating"
+          />
+          <label class="label" for="rating-poor">Poor</label>
+        </div>
+
+        <div>
+          <input
+            type="radio"
+            name="rating"
+            id="rating-avg"
+            value="average"
+            v-model="rating"
+          />
+          <label class="label" for="rating-avg">Average</label>
+        </div>
+
+        <div>
+          <input
+            type="radio"
+            name="rating"
+            id="rating-great"
+            value="great"
+            v-model="rating"
+          />
+          <label class="label" for="rating-great">Great</label>
+        </div>
+        <p v-if="invalidRatingInput" class="text-red-500">
+          Please choose your learning experience!
+        </p>
+      </base-card>
+
+      <base-button
+        bgcolor="bg-purple-900"
+        txtcolor="text-white"
+        label="Submit"
+      ></base-button>
+    </form>
+    <div class="container">
+      <base-card>
+        <ul v-for="result in surveyResults" :key="result.id">
+          <li>
+            <span class="text-purple-600 italic">{{ result.name }}</span>
+            rated the learning experience
+            <span class="text-green-600 italic">{{ result.rating }}</span>
+          </li>
+        </ul>
+      </base-card>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      enteredName: "",
+      rating: null,
+      invalidNameInput: false,
+      invalidRatingInput: false,
+      surveyResults: [
+        // {
+        //   id: 1,
+        //   name: "Umaporn",
+        //   rating: "Great",
+        // },
+        // {
+        //   id: 2,
+        //   name: "Suda",
+        //   rating: "Average",
+        // },
+      ],
+    };
+  },
+  props: {
+    fetchSurveyResults: Object,
+  },
+  methods: {
+    submitForm() {
+      this.invalidNameInput = this.enteredName === "" ? true : false;
+      this.invalidRatingInput = this.rating === null ? true : false;
+      console.log(`${this.invalidNameInput}  :  ${this.invalidRatingInput}`);
+
+      if ((this.invalidNameInput && this.invalidRatingInput) === false) {
+        let entry = {
+          id: this.surveyResults.length + 1,
+          name: this.enteredName,
+          rating: this.rating,
+        };
+
+        // let newSurvey = [];
+        // newSurvey = [... this.surveyResults,entry]
+        this.surveyResults = [...this.surveyResults, entry];
+
+        //this.surveyResults.push(entry);
+        // console.log(`${this.surveyResults[this.surveyResults.length-1].id}`);
+        // console.log(`${this.surveyResults[this.surveyResults.length-1].name}`);
+        // console.log(`${this.surveyResults[this.surveyResults.length-1].rating}`);
+
+        this.$emit("survey-results", this.surveyResults);
+      }
+    },
+    validateName() {
+      this.invalidNameInput = this.enteredName === "" ? true : false;
+    },
+    async fetchSurveyResult() {
+      const res = await fetch("http://localhost:5000/surveyResults");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    this.surveyResults = await this.fetchSurveyResult();
+  },
+  //   watch: {
+  //     fetchSurveyResults() {
+  //       this.surveyResults = this.fetchSurveyResults;
+  //     },
+  //   },
+};
+</script>
